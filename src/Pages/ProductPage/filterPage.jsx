0babe85b0a10fb8bb578cf082bc8e1f80eba75b1
assala <ismailassala1@gmail.com>
2024-04-02 14:40:53 +0300

    changes

--- src/Pages/ProductPage/filterPage.jsx
+++ src/Pages/ProductPage/filterPage.jsx
@@ -2,36 +2,28 @@ import React, {useState, useEffect} from 'react';
 import "../ProductPage/filterPage.css";
 import Slider from "@mui/material/Slider";
 
-
-function FilterPage({ onClose, products, minPrice, maxPrice }) {
-
-
+function FilterPage({ onClose, products, minPrice, maxPrice, applyFilters }) {
     const [activeIndices, setActiveIndices] = useState([]);
-    const [range, setRange] = React.useState([minPrice, maxPrice]);
+    const [range, setRange] = useState([minPrice, maxPrice]);
+    const [selectedSizes, setSelectedSizes] = useState([]);
 
-
-    // Extract unique Colors
-    // Function to extract unique colors from products
     const extractUniqueColors = () => {
-        const uniqueColorsMap = new Map(); // Using a Map to ensure uniqueness based on color name
+        const uniqueColorsMap = new Map();
         if (Array.isArray(products)) {
             products.forEach(product => {
                 if (product.variations && Array.isArray(product.variations)) {
                     product.variations.forEach(variation => {
                         if (variation.color) {
-                            // Store color name as key and hex value as value in the Map
                             uniqueColorsMap.set(variation.color, variation.color_hex);
                         }
                     });
                 }
             });
         }
-        // Convert Map to an array of objects with name and hex properties
         return Array.from(uniqueColorsMap, ([name, hex]) => ({ name, hex }));
     };
-        const uniqueColors = extractUniqueColors();
+    const uniqueColors = extractUniqueColors();
 
-    // Extract unique Sizes
     const extractUniqueSizes = () => {
         const uniqueSizes = new Set();
         if (Array.isArray(products)) {
@@ -47,10 +39,9 @@ function FilterPage({ onClose, products, minPrice, maxPrice }) {
         }
         return Array.from(uniqueSizes);
     };
-        const uniqueSizes = extractUniqueSizes();
+    const uniqueSizes = extractUniqueSizes();
 
-
-    const handleClick = (index) => {
+    const handleClickSizes = (index) => {
         const indexExists = activeIndices.includes(index);
         if (indexExists) {
             setActiveIndices(activeIndices.filter((i) => i !== index));
@@ -59,64 +50,77 @@ function FilterPage({ onClose, products, minPrice, maxPrice }) {
         }
     };
 
-    //Price Range
+    const handleApplyFilters = () => {
+        const filters = {
+            sizes: selectedSizes.map((sizeIndex) => uniqueSizes[sizeIndex - 1]),
+            min_price: range[0],
+            max_price: range[1],
+        };
+        applyFilters(filters);
+        onClose();
+    };
+
     useEffect(() => {
         setRange([minPrice, maxPrice]);
     }, [minPrice, maxPrice]);
 
-
     function handleChanges(event, newValue) {
-       setRange(newValue);
+        setRange(newValue);
     }
 
     return (
-    <div className="filter-page">
+        <div className="filter-page">
+            <div className='clear-apply-filters'>
+                <button className='clear-filter' onClick={onClose}> Clear Filters </button>
+                <button className='apply-filter' onClick={handleApplyFilters}> Apply Filters</button>
+            </div>
 
-        <div className='clear-apply-filters'>
-            <button className='clear-filter' onClick={onClose}> Clear Filters </button>
-            <button className='apply-filter'> Apply Filters</button>
-        </div>
-
-        <div className='colors-div'>
-            <h4>Select Color</h4>
-            {uniqueColors.map((color, index) => (
-                <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
-                    <div className='color-swatch' style={{ backgroundColor: color.hex }}></div>
-                    <p className='products-color-name'>{color.name}</p>
-                </div>
-            ))}
-        </div>
-
-        <hr></hr>
-
-        <div className='sizes-div'>
-            <h4>Select Size</h4>
-            <div className='sizes'>
-                {uniqueSizes.map((size, index) => (
-                <p
-                    className={`products-size${activeIndices.includes(index) ? ' active' : ''}`}
-                    tabIndex="0"
-                    key={index}
-                    onClick={() => handleClick(index)}
-                >
-                    {size}
-                </p>
+            <div className='colors-div'>
+                <h4>Select Color</h4>
+                <div className='colors-columns'>
+                {uniqueColors.map((color, index) => (
+                    <div key={index} style={{ display: 'flex', alignItems: 'center', breakInside: 'avoid-column' }}>
+                        <p className='color-swatch' style={{ backgroundColor: color.hex }}></p>
+                        <p className='products-color-name'>{color.name}</p>
+                    </div>
                 ))}
+                </div>
             </div>
-        </div>
 
-        <hr></hr>
+            <hr></hr>
 
-        <div className="price-div">
-            <h4>Price</h4>
-            <div className='price-slider-div'>
-                <h4>{minPrice}</h4>
-                <Slider value={range} onChange={handleChanges} valueLabelDisplay="auto" className="price-slider" min={parseInt(minPrice)} max={parseInt(maxPrice)} />
-                <h4>{maxPrice}</h4>
+            <div className='sizes-div'>
+                <h4>Select Size</h4>
+                <div className='sizes'>
+                    {uniqueSizes.map((size, index) => (
+                        <p
+                            className={`products-size${activeIndices.includes(index) ? ' active' : ''}`}
+                            tabIndex="0"
+                            key={index}
+                            onClick={() => {
+                                handleClickSizes(index);
+                                setSelectedSizes(activeIndices.map(index => index + 1)); //Updated selected sizes
+                            }}
+                        >
+                            {size}
+                        </p>
+                    ))}
+                </div>
             </div>
-        </div>
 
-    </div>
+            <hr></hr>
+
+            <div className="price-div">
+                <h4>Price</h4>
+                <div className='price-slider-div'>
+                    <h4>{minPrice}</h4>
+                    <Slider value={range} onChange={(_, newValue) => setRange(newValue)} valueLabelDisplay="auto"
+                            className="price-slider" min={parseInt(minPrice)} max={parseInt(maxPrice)} />
+                    <h4>{maxPrice}</h4>
+                </div>
+            </div>
+
+        </div>
     );
 }
 
--- src/Pages/ProductPage/productPage.jsx
+++ src/Pages/ProductPage/productPage.jsx
@@ -2,123 +2,144 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import "../ProductPage/productPage.css";
-import FilterPage from './filterPage';
+import FilterPage from '../ProductPage/filterPage';
 
 function ProductPage() {
-const [productsData, setProductsData] = useState({});
-const { categoryId } = useParams();
-const [page, setPage] = useState(1);
-const [loading, setLoading] = useState(false);
-const [fetching, setFetching] = useState(false);
-const [sort, setSort] = useState('date-new-to-old');
-const [showFilter, setShowFilter] = useState(false);
+    const [productsData, setProductsData] = useState({});
+    const { categoryId } = useParams();
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [fetching, setFetching] = useState(false);
+    const [sort, setSort] = useState('date-new-to-old');
+    const [showFilter, setShowFilter] = useState(false);
+    const [filters, setFilters] = useState({
+        sizes: [],
+        min_price: null,
+        max_price: null,
+    });
 
-const handleFilterClick = () => {
-   setShowFilter(true);
-   document.body.classList.add('no-scroll');
-};
+    const handleFilterClick = () => {
+        setShowFilter(true);
+        document.body.classList.add('no-scroll');
+    };
 
-const handleCloseFilter = () => {
-   setShowFilter(false);
-   document.body.classList.remove('no-scroll');
-};
+    const handleCloseFilter = () => {
+        setShowFilter(false);
+        document.body.classList.remove('no-scroll');
+    };
 
-useEffect(() => {
-   fetchProducts();
-   }, [categoryId, page, sort]);
+    const applyFilters = (selectedFilters) => {
+        setFilters(selectedFilters);
+        setPage(1);
+    };
 
-const fetchProducts = async () => {
-   if (fetching) return;
-   setFetching(true);
-   setLoading(true);
-    try {
-        const response = await axios.get(`https://app.kyveli.me/api/products/${categoryId}?page=${page}&sort=${sort}`);
-        const newData = response.data.data;
-        if (page === 1) {
-        setProductsData({
+
+    useEffect(() => {
+        fetchProducts();
+    }, [categoryId, page, sort, filters]);
+
+    const fetchProducts = async () => {
+        if (fetching) return;
+        setFetching(true);
+        setLoading(true);
+        try {
+            const apiUrl = `https://app.kyveli.me/api/products/${categoryId}?page=${page}&sort=${sort}`;
+            console.log("API URL:", apiUrl);
+
+            const response = await axios.get(apiUrl, {
+                params: filters
+            });
+            console.log("API Response:", response.data);
+
+            const newData = response.data.data;
+            if (page === 1) {
+                setProductsData({
                     products: newData.products,
                     product_count: newData.product_count,
                     subcategory: newData.subcategory,
                     minPrice: newData.minPrice,
                     maxPrice: newData.maxPrice,
-                 });
-        } else {
+                });
+            } else {
                 setProductsData(prevData => ({
-                ...prevData,
-                products: [...prevData.products, ...newData.products],
-                product_count: newData.product_count,
-                subcategory: newData.subcategory
-            }));
+                    ...prevData,
+                    products: [...prevData.products, ...newData.products],
+                    product_count: newData.product_count,
+                    subcategory: newData.subcategory
+                }));
+            }
+        } catch (error) {
+            console.error('Error fetching products: ', error);
+        } finally {
+            setLoading(false);
+            setFetching(false);
         }
-    } catch (error) {
-      console.error('Error fetching products: ', error);
-    } finally {
-        setLoading(false);
-        setFetching(false);
-    }
-};
-
-const handleScroll = () => {
-    if (
-        window.innerHeight + document.documentElement.scrollTop ===
-        document.documentElement.offsetHeight
-    ) {
-   setPage(prevPage => prevPage + 1);
-    }
- };
-
-useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-    window.removeEventListener('scroll', handleScroll);
     };
-}, []);
 
-const handleSortChange = (event) => {
-    setSort(event.target.value);
-    setPage(1);
-};
+    const handleScroll = () => {
+        if (
+            window.innerHeight + document.documentElement.scrollTop ===
+            document.documentElement.offsetHeight
+        ) {
+            setPage(prevPage => prevPage + 1);
+        }
+    };
 
-return (
-<div className='productPage'>
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
-    <div className={`filter-overlay ${showFilter ? 'show' : '' }`}>
-        <div className="filter-container">
-            <FilterPage onClose={handleCloseFilter} products={productsData.products}  minPrice={productsData.minPrice} maxPrice={productsData.maxPrice}/>
-        </div>
-    </div>
+    const handleSortChange = (event) => {
+        setSort(event.target.value);
+        setPage(1);
+    };
 
-    <div className='subcategory-count'>
-        <p className='products-subcategory'>{productsData.subcategory} <span
-                className='products-count'>{productsData.product_count} PRODUCTS</span></p>
-        <div>
-            <button className='products-filter-button' onClick={handleFilterClick}>FILTERS</button>
-            <span className='sort-label'>SORT BY:</span>
-            <select name="sort" id="sort" className='sort-select' onChange={handleSortChange} value={sort}>
-                <option value="date-new-to-old">New To Old</option>
-                <option value="date-old-to-new">Old To New</option>
-                <option value="price-high-to-low">Price High To Low</option>
-                <option value="price-low-to-high">Price Low To High</option>
-            </select>
-        </div>
-    </div>
-
-    <div className="product-container">
-        {Array.isArray(productsData.products) && productsData.products.map((product, index) => (
-        <div key={index}>
-            <div>
-                <div>
-                    <img src={product.images[0]} className='products-img' alt={`Product ${index}`} />
-                    <p className='products-websitename'>{product.website_name}</p>
-                    <p className='products-price'>{product.price}$</p>
+    return (
+        <div className='productPage'>
+            <div className={`filter-overlay ${showFilter ? 'show' : '' }`}>
+                <div className="filter-container">
+                    <FilterPage onClose={handleCloseFilter}
+                    products={productsData.products}
+                    minPrice={productsData.minPrice}
+                    maxPrice={productsData.maxPrice}
+                    applyFilters={applyFilters} />
                 </div>
             </div>
+            <div className='subcategory-count'>
+                <p className='products-subcategory'>{productsData.subcategory} <span
+                    className='products-count'>{productsData.product_count} PRODUCTS</span></p>
+                <div>
+                    <button className='products-filter-button' onClick={handleFilterClick}>
+                    FILTERS
+                    </button>
+                    <span className='sort-label'>SORT BY:</span>
+                    <select name="sort" id="sort" className='sort-select' onChange={handleSortChange} value={sort}>
+                        <option value="date-new-to-old">New To Old</option>
+                        <option value="date-old-to-new">Old To New</option>
+                        <option value="price-high-to-low">Price High To Low</option>
+                        <option value="price-low-to-high">Price Low To High</option>
+                    </select>
+                </div>
+            </div>
+            <div className="product-container">
+                {Array.isArray(productsData.products) && productsData.products.map((product, index) => (
+                    <div key={index}>
+                        <div>
+                            <div>
+                                <img src={product.images[0]} className='products-img' alt={`Product ${index}`} />
+                                <p className='products-websitename'>{product.website_name}</p>
+                                <p className='products-price'>{product.price}$</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            {loading && <p>Loading...</p>}
         </div>
-        ))}
-    </div>
-    {loading && <p>Loading...</p>}
-</div>
-);
+    );
 }
 
 export default ProductPage;
